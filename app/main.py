9a81e9500564d0c69d988961041ce55f98ce8b07
Rajdeep Das <rajdeepdaslimbo@gmail.com>
2026-04-23 17:17:44 +0530

    add root endpoint

--- app/main.py
+++ app/main.py
@@ -4,7 +4,12 @@ from routes import events, transactions, reconciliation
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Payment Reconciliation Service")
+app = FastAPI(title="Payment Service")
+
+@app.get("/")
+def root():
+    return {"message": "Payment Service is running"}
+
 
 app.include_router(events.router)
 app.include_router(transactions.router)
